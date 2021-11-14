0b7a35e5106d6dcc28bbdf559ef76ad36c3aed1f
essam omar <essam.93@hotmail.com>
2021-11-14 22:42:35 +0400

    11

---
+++ modular/driver.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const events = require('../events');
+
+events.on('in_transit', in_transit);
+
+function in_transit(payload) {
+    setTimeout(()=>{
+        console.log(`DRIVER: picked up ${payload.orderID}`);
+        console.log("EVENT",` event: 'in_transit',
+        time:${payload.time},
+        payload: {
+        store: ${payload.store},
+        orderID: ${payload.orderID},
+        customer: ${payload.customer},
+        address: ${payload.address},`);
+    },1000)
+}
